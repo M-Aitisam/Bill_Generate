44f8b14ba78c249616c8dd91f5c734acbe7b7a1e
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-06 13:55:19 +0500

    update all code

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using Blazored.LocalStorage;
 using Bill_Generate.Models;
+using Bill_Generate.Pages;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<BillService>();
+
 
 
 var app = builder.Build();
--- Services/BillService.cs
+++ Services/BillService.cs
@@ -1,38 +1,25 @@
 ﻿using Bill_Generate.Pages;
+using System.Collections.Generic;
+using System.Linq;
 
 public class BillService
 {
-    private List<RateList.RateItem> _selectedItems = new List<RateList.RateItem>();
-
-    public IReadOnlyList<RateList.RateItem> SelectedItems => _selectedItems.AsReadOnly();
+    public List<RateList.RateItem> SelectedItems { get; private set; } = new List<RateList.RateItem>();
 
     public void AddItem(RateList.RateItem item)
     {
-        var existingItem = _selectedItems.FirstOrDefault(i => i.Name == item.Name);
-        if (existingItem == null)
-        {
-            _selectedItems.Add(item);
-        }
-        else
-        {
-            existingItem.Price = item.Price; // Optionally update the price
-        }
+        SelectedItems.Add(item);
     }
 
     public void RemoveItem(RateList.RateItem item)
     {
-        _selectedItems.Remove(item);
+        SelectedItems.Remove(item);
     }
 
-    public decimal TotalAmount => _selectedItems.Sum(item => item.Price);
-
-    public void LoadItems(List<RateList.RateItem> items)
-    {
-        _selectedItems = items ?? new List<RateList.RateItem>();
-    }
-
-    public void ClearItems()
-    {
-        _selectedItems.Clear();
-    }
+    public decimal TotalAmount => SelectedItems.Sum(item => item.Price);
+}
+public class RateItem
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
 }
