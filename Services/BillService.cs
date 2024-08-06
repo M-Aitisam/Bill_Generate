186bea46cc3127ce5f7868f4f046410267f3dcf9
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-06 11:44:38 +0500

    Add the function  when  click  on the item it can automatically   clear the bill

--- Services/BillService.cs
+++ Services/BillService.cs
@@ -1,20 +1,38 @@
 ﻿using Bill_Generate.Pages;
-using System.Collections.Generic;
-using System.Linq;
 
 public class BillService
 {
-    public List<RateList.RateItem> SelectedItems { get; private set; } = new List<RateList.RateItem>();
+    private List<RateList.RateItem> _selectedItems = new List<RateList.RateItem>();
+
+    public IReadOnlyList<RateList.RateItem> SelectedItems => _selectedItems.AsReadOnly();
 
     public void AddItem(RateList.RateItem item)
     {
-        SelectedItems.Add(item);
+        var existingItem = _selectedItems.FirstOrDefault(i => i.Name == item.Name);
+        if (existingItem == null)
+        {
+            _selectedItems.Add(item);
+        }
+        else
+        {
+            existingItem.Price = item.Price; // Optionally update the price
+        }
     }
 
     public void RemoveItem(RateList.RateItem item)
     {
-        SelectedItems.Remove(item);
+        _selectedItems.Remove(item);
     }
 
-    public decimal TotalAmount => SelectedItems.Sum(item => item.Price);
+    public decimal TotalAmount => _selectedItems.Sum(item => item.Price);
+
+    public void LoadItems(List<RateList.RateItem> items)
+    {
+        _selectedItems = items ?? new List<RateList.RateItem>();
+    }
+
+    public void ClearItems()
+    {
+        _selectedItems.Clear();
+    }
 }
