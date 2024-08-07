5aa8acdad9bb1420358e081f47e4d327ec9ca79c
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-07 13:48:38 +0500

    Add Invoke Async method to  automatically  update the  code

--- Services/BillService.cs
+++ Services/BillService.cs
@@ -1,30 +1,35 @@
 ﻿using Bill_Generate.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public class BillService
 {
     public List<RateList.RateItem> SelectedItems { get; private set; } = new List<RateList.RateItem>();
+    public decimal TotalAmount => SelectedItems.Sum(item => item.Price);
+
+    public event Action OnChange;
 
     public void AddItem(RateList.RateItem item)
     {
         SelectedItems.Add(item);
+        NotifyStateChanged();
     }
 
     public void RemoveItem(RateList.RateItem item)
     {
         SelectedItems.Remove(item);
+        NotifyStateChanged();
     }
 
-    public decimal TotalAmount => SelectedItems.Sum(item => item.Price);
-
-    public List<RateList.RateItem> GetSavedItems()
-    {
-        // Return the list of selected items
-        return SelectedItems;
-    }
     public void ClearAllItems()
     {
         SelectedItems.Clear();
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged()
+    {
+        OnChange?.Invoke();
     }
 }
