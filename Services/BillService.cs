ad8c8db05dbf641eb48b5df8313f54d889a8af9c
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-15 13:50:07 +0500

    styling the code

--- Services/BillService.cs
+++ Services/BillService.cs
@@ -1,49 +1,31 @@
 ﻿using Bill_Generate.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class BillService
 {
     public List<RateList.RateItem> SelectedItems { get; private set; } = new List<RateList.RateItem>();
 
-    // Calculate the total amount by summing up the prices of selected items
     public decimal TotalAmount => SelectedItems.Sum(item => item.Price);
 
     public event Action? OnChange;
 
     public void AddItem(RateList.RateItem item)
     {
-        if (!SelectedItems.Any(i => i.Name == item.Name))
+        var existingItem = SelectedItems.FirstOrDefault(i => i.Name == item.Name);
+        if (existingItem != null)
+        {
+            existingItem.Quantity++;
+            existingItem.Price = existingItem.BasePrice * existingItem.Quantity;
+        }
+        else
         {
             item.BasePrice = item.Price; // Store the original price as BasePrice
             item.Quantity = 1; // Set the initial quantity to 1
             SelectedItems.Add(item);
         }
-        else
-        {
-            IncrementItemQuantity(item);
-        }
         NotifyStateChanged();
-    }
-
-    public void IncrementItemQuantity(RateList.RateItem item)
-    {
-        var selectedItem = SelectedItems.FirstOrDefault(i => i.Name == item.Name);
-        if (selectedItem != null)
-        {
-            selectedItem.Quantity++;
-            selectedItem.Price = selectedItem.BasePrice * selectedItem.Quantity;
-            NotifyStateChanged();
-        }
-    }
-
-    public void UpdateItem(RateList.RateItem updatedItem)
-    {
-        var item = SelectedItems.FirstOrDefault(i => i.Name == updatedItem.Name);
-        if (item != null)
-        {
-            item.Quantity = updatedItem.Quantity;
-            item.Price = item.BasePrice * item.Quantity;
-            NotifyStateChanged();
-        }
     }
 
     public void RemoveItem(RateList.RateItem item)
@@ -56,6 +38,26 @@ public class BillService
     {
         SelectedItems.Clear();
         NotifyStateChanged();
+    }
+
+    public void ClearTotalAmount()
+    {
+        foreach (var item in SelectedItems)
+        {
+            item.Price = 0;
+        }
+        NotifyStateChanged();
+    }
+
+    public void UpdateItem(RateList.RateItem updatedItem)
+    {
+        var item = SelectedItems.FirstOrDefault(i => i.Name == updatedItem.Name);
+        if (item != null)
+        {
+            item.Quantity = updatedItem.Quantity;
+            item.Price = item.BasePrice * item.Quantity;
+            NotifyStateChanged();
+        }
     }
 
     private void NotifyStateChanged()
